79009c0d85e26a7e605010d667dea95c1006205e
Anurag <kranurag2004@gmail.com>
2024-08-14 20:35:20 +0530

    added problemset

--- recursion/subsetSum.java
+++ recursion/subsetSum.java
@@ -1,3 +1,30 @@
+/*Subset Sums
+Difficulty: MediumAccuracy: 72.55%Submissions: 123K+Points: 4
+Given a list arr of n integers, return sums of all subsets in it. Output sums can be printed in any order.
+
+ 
+
+Example 1:
+
+Input:
+n = 2
+arr[] = {2, 3}
+Output:
+0 2 3 5
+Explanation:
+When no elements is taken then Sum = 0.
+When only 2 is taken then Sum = 2.
+When only 3 is taken then Sum = 3.
+When element 2 and 3 are taken then 
+Sum = 2+3 = 5.
+Example 2:
+
+Input:
+n = 3
+arr = {5, 2, 1}
+Output:
+0 1 2 3 5 6 7 8 */
+
 import java.util.ArrayList;
 import java.util.Collections;
 
